db53a0f806c9d762e06d05b7707bda5bc8aa979e
Faris Janjua <farisjanjua@ucalgary.ca>
2025-03-14 12:52:44 -0600

    up until page 11

---
+++ tic-tac-toe/src/App.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function Square() {
+
+  const [value, setValue] = useState(null);
+
+  function handleClick() {
+    setValue("X"); 
+  }
+
+  return (
+    <button className="square" onClick={handleClick}>
+      {value}
+    </button>
+  );
+}
+
+export default function Board() {
+
+  const[ squares, setSquares ] = useState(Array(9).fill(null));
+  return (
+    <>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </>
+  );
+}
